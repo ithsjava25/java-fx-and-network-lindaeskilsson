4ce98d3a86f7bd1ef0ceb95d25fb98b59ba84629
Linda Eskilsson <eskilsson.linda@hotmail.se>
2025-11-03 13:15:53 +0100

    created wack program

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
 
 /**
  * Controller layer: mediates between the view (FXML) and the model.
@@ -11,13 +16,41 @@ public class HelloController {
     private final HelloModel model = new HelloModel();
 
     @FXML
-    private Label messageLabel;
+    private Text messageLabel;
 
     @FXML
     private void initialize() {
-
+        messageLabel.textProperty().bind(
+                Bindings.concat(model.pointsProperty().asString(), " poäng")
+        );
+        startNextEvent();
     }
+
+    private void startNextEvent() {
+        new Timeline(
+                new KeyFrame(
+                        Duration.millis(3000 * Math.random()), event -> {
+                    model.changeImage(
+                            (int) (3 * Math.random()),
+                            (int) (4 * Math.random()));
+                    startNextEvent();
+                }
+                )).play();
+    }
+
     public HelloModel getModel() {
         return model;
+    }
+
+    public void image1(MouseEvent mouseEvent) {
+        model.smash(0);
+    }
+
+    public void image2(MouseEvent mouseEvent) {
+        model.smash(1);
+    }
+
+    public void image3(MouseEvent mouseEvent) {
+        model.smash(2);
     }
 }
--- src/main/java/com/example/HelloModel.java
+++ src/main/java/com/example/HelloModel.java
@@ -1,5 +1,9 @@
 package com.example;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.image.Image;
 
 /**
@@ -7,17 +11,117 @@ import javafx.scene.image.Image;
  */
 public class HelloModel {
 
-    Image noSmash;
-    Image smash1;
-    Image smash2;
-    Image smash3;
-    Image smash4;
+    private final Image noSmash;
+    private final Image smash1;
+    private final Image smash2;
+    private final Image smash3;
+    private final Image smash4;
+
+    private final ObjectProperty<Image> first;
+    private final ObjectProperty<Image> second;
+    private final ObjectProperty<Image> third;
+
+    private IntegerProperty points;
 
     public HelloModel() {
-        noSmash = new Image(getClass().getResource("/lantern.png").toExternalForm());
-    }
-    public Image getNoSmash() {
-        return noSmash;
+        noSmash = new Image(getClass()
+                .getResource("/lantern.png")
+                .toExternalForm());
+        smash1 = new Image(getClass()
+                .getResource("/lantern1.png")
+                .toExternalForm());
+        smash2 = new Image(getClass()
+                .getResource("/lantern2.png")
+                .toExternalForm());
+        smash3 = new Image(getClass()
+                .getResource("/lantern3.png")
+                .toExternalForm());
+        smash4 = new Image(getClass()
+                .getResource("/lantern4.png")
+                .toExternalForm());
+        first = new SimpleObjectProperty<>(noSmash);
+        second = new SimpleObjectProperty<>(noSmash);
+        third = new SimpleObjectProperty<>(noSmash);
+
+        points = new SimpleIntegerProperty(0);
     }
 
+    public Image getFirst() {
+        return first.get();
+    }
+
+    public ObjectProperty<Image> firstProperty() {
+        return first;
+    }
+
+    public void setFirst(Image first) {
+        this.first.set(first);
+    }
+
+    public Image getSecond() {
+        return second.get();
+    }
+
+    public ObjectProperty<Image> secondProperty() {
+        return second;
+    }
+
+    public void setSecond(Image second) {
+        this.second.set(second);
+    }
+
+    public Image getThird() {
+        return third.get();
+    }
+
+    public ObjectProperty<Image> thirdProperty() {
+        return third;
+    }
+
+    public void setThird(Image third) {
+        this.third.set(third);
+    }
+
+    public int getPoints() {
+        return points.get();
+    }
+
+    public IntegerProperty pointsProperty() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points.set(points);
+    }
+
+    public void smash(int lantern) {
+        if (lantern == 0 && getFirst() != noSmash) {
+            setPoints(getPoints() + 1);
+            setFirst(noSmash);
+        } else if (lantern == 1 && getSecond() != noSmash) {
+            setPoints(getPoints() + 1);
+            setSecond(noSmash);
+        } else if (lantern == 2 && getThird() != noSmash) {
+            setPoints(getPoints() + 1);
+            setThird(noSmash);
+        }
+    }
+
+    public void changeImage(int lantern, int imageId) {
+        switch (lantern) {
+            case 0 -> setFirst(getImage(imageId));
+            case 1 -> setSecond(getImage(imageId));
+            case 2 -> setThird(getImage(imageId));
+        }
+    }
+
+    private Image getImage(int imageId) {
+        return switch (imageId) {
+            case 0 -> smash1;
+            case 1 -> smash2;
+            case 2 -> smash3;
+            case 3 -> smash4;
+            default -> noSmash;
+        };
+    }
 }
